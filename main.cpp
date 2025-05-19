1fb53037b7ceff35f0d3015b4cf5f8d12102075b
iamkira420 <lmboniswa01@gmail.com>
2025-05-19 22:15:50 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "This is the start of the Library Management System" << endl;
+    return 0;
+}
